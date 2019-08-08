4ffb0b9f68d0c4412538bffe5148e1dae6531c5f
Андрій <Andrii Hromov>
2019-08-08 15:40:52 +0300

    Added sh run-scripts

--- src/main/resources/freebsd/GameServer_loop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pidfile=$1
-
-[ -f log/java0.log.0 ] && mv log/java0.log.0 "log/`date +%Y-%m-%d_%H-%M-%S`_java.log"
-[ -f log/stdout.log ] && mv log/stdout.log "log/`date +%Y-%m-%d_%H-%M-%S`_stdout.log"
-/usr/local/openjdk11/bin/java -Xms512m -Xmx2g -jar l2jserver.jar > log/stdout.log &
-echo $! > ${pidfile}

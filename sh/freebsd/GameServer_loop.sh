5a46a8d60d4461e8b80147bd277df3fa58ec4281
andri <andri@DESKTOP-EVQGO3R>
2020-06-04 13:14:03 +0300

    Moved to l2login project

--- sh/freebsd/GameServer_loop.sh
+++ sh/freebsd/GameServer_loop.sh
@@ -1,13 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
-# exit codes of GameServer:
-#  0 normal shutdown
-#  2 reboot attempt
+pidfile=$1
+user=$2
 
-while :; do
-	[ -f log/java0.log.0 ] && mv log/java0.log.0 "log/`date +%Y-%m-%d_%H-%M-%S`_java.log"
-	[ -f log/stdout.log ] && mv log/stdout.log "log/`date +%Y-%m-%d_%H-%M-%S`_stdout.log"
-	java -Xms512m -Xmx2g -jar l2jserver.jar > log/stdout.log 2>&1
-	[ $? -ne 2 ] && break
-	sleep 10
-done
+[ -f log/stdout.log ] && /usr/bin/gzip log/stdout.log && mv log/stdout.log.gz "log/`date +%Y-%m-%d_%H-%M-%S`_stdout.log.gz"
+/usr/sbin/daemon -u ${user} /usr/local/openjdk11/bin/java -Xms512m -Xmx2g -jar l2jserver.jar > log/stdout.log & 
+let pid=$!+1
+
+echo ${pid} > ${pidfile}
